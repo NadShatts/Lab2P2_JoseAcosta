80a28125404d12b68aa9a174bfe9bfefec918e09
NadShatts <josedaniel202006@gmail.com>
2024-01-26 15:52:53 -0600

    Septimo Avance

--- Lab2P2_JoseAcosta/src/lab2p2_joseacosta/Lab2P2_JoseAcosta.java
+++ Lab2P2_JoseAcosta/src/lab2p2_joseacosta/Lab2P2_JoseAcosta.java
@@ -16,22 +16,20 @@ public class Lab2P2_JoseAcosta {
     /**
      * @param args the command line arguments
      */
-    
     public static void main(String[] args) {
 
         ArrayList recursos = new ArrayList<>();
-        
-    ArrayList<Usuario> usuarios = new ArrayList<>();
+
+        ArrayList<Usuario> usuarios = new ArrayList<>();
         Scanner entrada = new Scanner(System.in);
         int opcion;
 
-        
         usuarios.add(new Usuario("estudiante", "123", "estudiante"));
         usuarios.add(new Usuario("profesor", "456", "profesor"));
         usuarios.add(new Usuario("bibliotecario", "789", "bibliotecario"));
 
-
         Scanner s = new Scanner(System.in);
+        System.out.println("estudiante, profesor, bibliotecario");
         System.out.print("Ingrese su nombre de usuario: ");
         String nombreUsuario = s.next();
         System.out.print("Ingrese su contraseña: ");
@@ -50,9 +48,9 @@ public class Lab2P2_JoseAcosta {
             System.out.println("Nombre de usuario o contraseña incorrectos. El programa se cerrará.");
             return;
         }
-        
+
         do {
-            System.out.println("Bienvenido " +nombreUsuario);
+            System.out.println("Bienvenido " + nombreUsuario);
             System.out.println("1. Listar Recurso.");
             System.out.println("2. Crear Recurso");
             System.out.println("3. Eliminar Recurso.");
@@ -64,33 +62,33 @@ public class Lab2P2_JoseAcosta {
                 case 1:
 
                     if (usuarioActual.tipoUsuario.equals("estudiante") || usuarioActual.tipoUsuario.equals("profesor") || usuarioActual.tipoUsuario.equals("bibliotecario")) {
-                    
-                        if(recursos.isEmpty()){
-                        System.out.println("No hay nada ingresado hasta el momento una disculpa");
-                    }else{
-    for (Object recurso : recursos) {
-         System.out.println("Lista de recursos en la biblioteca virtual:");
-         System.out.println(recurso.toString());
-    
-    }
-        
-    }
-} else {
-    System.out.println("No tiene permisos para listar recursos. Esta opción es solo para estudiantes y profesores.");
-}
+
+                        if (recursos.isEmpty()) {
+                            System.out.println("No hay nada ingresado hasta el momento una disculpa");
+                        } else {
+                            for (Object recurso : recursos) {
+                                System.out.println("Lista de recursos en la biblioteca virtual:");
+                                System.out.println(recurso.toString());
+
+                            }
+
+                        }
+                    } else {
+                        System.out.println("No tiene permisos para listar recursos. Esta opción es solo para estudiantes y profesores.");
+                    }
                     break;
 
                 case 2:
                     if (usuarioActual.tipoUsuario.equals("bibliotecario") || usuarioActual.tipoUsuario.equals("profesor")) {
-                    Scanner entrada1 = new Scanner(System.in);
-                    System.out.println("Seleccione el tipo de recurso a crear:");
-                    System.out.println("1. Libro");
-                    System.out.println("2. Artículo");
-                    System.out.println("3. Curso en Línea");
-                    System.out.println("4. Conferencia Virtual");
-                    System.out.print("Ingrese su elección: ");
+                        Scanner entrada1 = new Scanner(System.in);
+                        System.out.println("Seleccione el tipo de recurso a crear:");
+                        System.out.println("1. Libro");
+                        System.out.println("2. Artículo");
+                        System.out.println("3. Curso en Línea");
+                        System.out.println("4. Conferencia Virtual");
+                        System.out.print("Ingrese su elección: ");
                     } else {
-    System.out.println("No tiene permisos para hacer eso.");
+                        System.out.println("No tiene permisos para hacer eso.");
                     }
                     Scanner entrada1 = new Scanner(System.in);
                     int tipoRecurso = entrada1.nextInt();
@@ -98,46 +96,43 @@ public class Lab2P2_JoseAcosta {
                         case 1:
                             System.out.print("Ingrese el título del libro: ");
                             String tituloLibro = entrada1.next();
-                            System.out.println("Ingrese el autor del libro:");
+                            System.out.print("Ingrese el autor del libro:");
                             String autorLibro = entrada1.next();
-                            System.out.println("Ingrese el genero del libro:");
+                            System.out.print("Ingrese el genero del libro:");
                             String generoLibro = entrada1.next();
-                            System.out.println("Ingrese el año de publicacion del libro: ");
+                            System.out.print("Ingrese el año de publicacion del libro: ");
                             int añoLibro = entrada1.nextInt();
-                            System.out.println("Disponibilidad");
-                    
-                            
-                              Libros nuevoLibro = new Libros(tituloLibro, autorLibro, generoLibro, añoLibro);
-recursos.add(nuevoLibro);
+                            System.out.print("Disponibilidad");
+
+                            Libros nuevoLibro = new Libros(tituloLibro, autorLibro, generoLibro, añoLibro);
+                            recursos.add(nuevoLibro);
                             System.out.println("Libro ingresado correctamente.");
-                    
-        
+
                             break;
                         case 2:
                             System.out.print("Ingrese el título del artículo: ");
                             String tituloArticulo = entrada1.next();
-                            System.out.println("Ingrese el autor del articulo: ");
+                            System.out.print("Ingrese el autor del articulo: ");
                             String autorArticulo = entrada1.next();
-                            System.out.println("Ingrese el tema del articulo: ");
+                            System.out.print("Ingrese el tema del articulo: ");
                             String temaArticulo = entrada1.next();
-                            System.out.println("Ingrese la fecha del articulo: ");
+                            System.out.print("Ingrese la fecha del articulo: ");
                             String fechaArticulo = entrada1.next();
-                            
-                            Articulos nuevoArticulo = new Articulos(tituloArticulo, autorArticulo,  temaArticulo, fechaArticulo);
+
+                            Articulos nuevoArticulo = new Articulos(tituloArticulo, autorArticulo, temaArticulo, fechaArticulo);
                             recursos.add(nuevoArticulo);
                             System.out.println("Articulo ingresado correctamente.");
                             break;
                         case 3:
                             System.out.print("Ingrese el título del curso en línea: ");
-                            String tituloCurso =entrada1.next();
-                            System.out.println("Ingrese el instructor del curso:");
+                            String tituloCurso = entrada1.next();
+                            System.out.print("Ingrese el instructor del curso:");
                             String instructorCurso = entrada1.next();
-                            System.out.println("Ingrese la duracion: ");
+                            System.out.print("Ingrese la duracion: ");
                             int duracionCurso = entrada1.nextInt();
-                            System.out.println("Ingrese la plataforma: ");
+                            System.out.print("Ingrese la plataforma: ");
                             int plataformaCurso = entrada1.nextInt();
-                            
-                            
+
                             CursosEnLinea curso = new CursosEnLinea(tituloCurso, instructorCurso, duracionCurso, plataformaCurso);
                             recursos.add(curso);
                             System.out.println("Curso ingresado correctamente.");
@@ -146,15 +141,15 @@ recursos.add(nuevoLibro);
 
                             System.out.print("Ingrese el título de la conferencia virtual: ");
                             String tituloConferencia = entrada1.next();
-                            System.out.println("Ingrese el conferencista:");
+                            System.out.print("Ingrese el conferencista:");
                             int conferencistaConferencia = entrada1.nextInt();
-                            System.out.println("Ingrese la fecha: ");
+                            System.out.print("Ingrese la fecha: ");
                             String fechaConferencia = entrada1.next();
-                            System.out.println("Ingrese el enlace de acceso: ");
+                            System.out.print("Ingrese el enlace de acceso: ");
                             String enlaceAcceso = entrada1.next();
-                            
+
                             ConferenciasVirtuales conferencia = new ConferenciasVirtuales(tituloConferencia, conferencistaConferencia, fechaConferencia, enlaceAcceso);
-recursos.add(conferencia);
+                            recursos.add(conferencia);
                             System.out.println("Conferencia ingresada correctamente");
 
                             break;
@@ -163,15 +158,17 @@ recursos.add(conferencia);
                     }
 
                     break;
-                    
+
                 case 3:
-                     if (usuarioActual.tipoUsuario.equals("bibliotecario")) {
-                     }else{
-                         System.out.println("No tienes permiso apra hacer eso.");
+                     if (usuarioActual.tipoUsuario.equals("bibliotecario") || usuarioActual.tipoUsuario.equals("profesor")) {
+                         System.out.println("Ingrese el recurso que quiera eliminar: ");
+        System.out.println("Ingrese el título del recurso a eliminar:");
+        String tituloEliminar = entrada.next();
+        
+
                      }
-                    break;
-                     
                 case 4:
+
                     break;
 
                 case 5:
